27a5646df689e8aace298c4f9df172014aaf02b6
DanielRmz1 <daniel.1912@hotmail.com>
2021-05-18 21:07:51 -0500

    Fix el login culero

--- Proyecto La Kanasta/Login.vb
+++ Proyecto La Kanasta/Login.vb
@@ -1,22 +1,17 @@
 ﻿Public Class Form3
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) 
-
-    End Sub 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim usuario As New ClaseLogin(TextBox1.Text, TextBox2.Text)
-        'Validamos si el usuario que intenta acceder al sistema está registrado
-        'éste usuario es un registro de la tabla de usuarios, NO ES el usuario
-        'de ORACLE, el el usuario final
-        If usuario.consultaUsuario() = False Then
-            MsgBox("Usuario no registrado, favor de llamar al ADMINISTRADOR del sistema", MsgBoxStyle.Exclamation)
-            cnx.Close()
-        Else
-            ' si está registrado el usuario, cerramos la conexión a la BD
-            cnx.Close()
-            ' presentamos la pantalla del Menú del sistema
-            MenuUsuarios.Show()
-            ' descargamos la pantalla actual LOGIN
+        Dim usuario = txtUser.Text
+        Dim pass = txtPass.Text
+        Dim cnx = New Conexion()
+        cnx.Consulta($"SELECT Usuario, Contraseña FROM Empleados WHERE Usuario ='{usuario}' AND Contraseña = '{pass}'", "Usuarios")
+        If cnx.ds.Tables("Usuarios").Rows.Count > 0 Then
+            Dim frm = New Ventas()
+            frm.Location = Me.Location
+            frm.StartPosition = FormStartPosition.Manual
+            frm.Show()
             Me.Hide()
+        Else
+            MsgBox("No Logeado jajaja")
         End If
     End Sub
 End Class
